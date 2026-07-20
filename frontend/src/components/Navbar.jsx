414fd0e4c1238dda94d34e5e3087176ab866e997
Noyan.dev <ma13842791199@gmail.com>
2026-07-20 13:45:04 +0330

    new navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,24 +2,54 @@ import "./../styles/navbar.css";
 import Button from "./Button";
 import Logo from "../assets/icons/icon.png";
 
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+
+            setScrolled(window.scrollY > 30);
+
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+
+    }, []);
+
     return (
-        <header className="navbar">
+
+        <header className={`navbar ${scrolled ? "scrolled" : ""}`}>
+
             <div className="navbar-container">
-                <div className="logo" >
+
+                <a href="/" className="logo">
+
                     <img src={Logo} alt="Alma" />
+
                     <span>Alma</span>
-                </div>
-                
+
+                </a>
 
                 <nav className="nav-links">
+
                     <a href="#">Features</a>
-                    <a href="#">How it work</a>
+
+                    <a href="#">How it works</a>
+
                     <a href="#">Pricing</a>
+
                     <a href="#">About</a>
+
                 </nav>
 
                 <div className="nav-actions">
+
                     <Button variant="secondary">
                         Login
                     </Button>
@@ -27,10 +57,15 @@ const Navbar = () => {
                     <Button>
                         Get Started
                     </Button>
+
                 </div>
+
             </div>
+
         </header>
+
     );
+
 };
 
 export default Navbar;
